f984b689e99d9ce0327eea937034c1b93facfe26
nbxiglk <fibr3s@gmail.com>
2022-07-16 21:04:09 +0800

    Update sloth.sh

--- sloth.sh
+++ sloth.sh
@@ -28,7 +28,7 @@ sort $outputpath/dns_$1.txt | uniq >> $outputpath/domain_$1.txt
 sort $outputpath/domain_$1.txt | uniq | puredns resolve -q -r $resolverspath --write $outputpath/domain_$1.txt
 # naabu
 echo "Start naabu"
-naabu -list $outputpath/domain_$1.txt -ec -tp 1000 -nmap-cli "nmap -sV -oN $1_nmap.txt" -silent -o $outputpath/portscan_$1.txt
+naabu -list $outputpath/domain_$1.txt -ec -tp 1000 -nmap-cli "nmap -sV -oN $outputpath/$1_nmap.txt" -silent -o $outputpath/portscan_$1.txt
 # httpx
 echo "Start httpx"
 cat $outputpath/domain_$1.txt | httpx -ip -title -cname  -ports 80,443,8080,8443,7001,8081 -follow-host-redirects -location -no-color -random-agent -silent -web-server -status-code -tech-detect -o $outputpath/httpx-$1.txt
@@ -37,4 +37,4 @@ awk '{print $1}' $outputpath/httpx-$1.txt > $outputpath/scanurl.txt
 echo "Start nuclei"
 nuclei -l  $outputpath/scanurl.txt -eid $nucleiblack -o $outputpath/nucleiscan_$1.txt
 echo "Package ing"
-zip -r -q $1.zip $outputpath
+zip -r -q /root/result/$1.zip $outputpath
